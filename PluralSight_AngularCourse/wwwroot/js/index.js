ac5715a6e75dd374c94dc96b3622ba58cfe4f9a1
antonio <antoniomanzari@gmail.com>
2021-04-30 16:18:50 -0400

    finished chapter 4

---
+++ PluralSight_AngularCourse/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    console.log("JS file loaded!")
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("User Clicked Buy Button")
+    })
+
+    var productInfo = $(".products-props li");
+    productInfo.on("click", function () {
+        consoel.log("Ya clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(30);
+    });
+
+});
+
